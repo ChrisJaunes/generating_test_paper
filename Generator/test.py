911cf597dc5c1e5bcb897a0bbe6073720f7f26dc
ChrisJaunes <ChrisJaunes@gmail.com>
2021-04-27 19:31:53 +0800

    [feat] 修改了data_cleaning和test

--- Generator/test.py
+++ Generator/test.py
@@ -3,11 +3,11 @@ from data_cleaning import *
 
 
 def test_generator():
-    generator(r"C:\Users\Chris\Desktop\Java-题库\DataBase\problem_set_std1.CSV")
+    generator(r"..\DataBase\problem_set_std1.CSV")
 
 
 def test_data_cleaning():
-    data_cleaning(r"C:\Users\Chris\Desktop\Java-题库\DataBase\problem_set_java.xls")
+    data_cleaning(r"..\DataBase\problem_set_java.xls")
 
 
 if __name__ == '__main__':
